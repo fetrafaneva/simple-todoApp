a0b093fe1379bab07d34508709aeb7a73d24668a
2shining <fetrafaneva@gmail.com>
2026-05-31 20:22:26 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+type Priority = "Urgente" | "Moyenne" | "Basse";
+
+type Todo = {
+  id: number;
+  text: string;
+  priority: Priority;
+};
+
+function App() {
+  return <div className="bg-red-500"> test </div>;
+}
+
+export default App;
