9daa3de0dbb2c321e572448e569cf580a485d803
2shining <fetrafaneva@gmail.com>
2026-06-02 23:45:33 +0300

    feat: Add bouton filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type Priority = "Urgente" | "Moyenne" | "Basse";
 
@@ -11,7 +11,15 @@ type Todo = {
 function App() {
   const [input, setInput] = useState("");
   const [priority, setPriority] = useState<Priority>("Moyenne");
-  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const savedTodos = localStorage.getItem("todos");
+  const initialTodos = savedTodos ? JSON.parse(savedTodos) : [];
+  const [todos, setTodos] = useState<Todo[]>(initialTodos);
+  const [filter, setFilter] = useState<Priority | "Tous">("Tous");
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo() {
     if (input.trim() == "") {
@@ -30,6 +38,8 @@ function App() {
     setPriority("Moyenne");
     console.log(todos);
   }
+
+  let filteredTodos: Todo[] = [];
 
   return (
     <div className="flex justify-center">
@@ -58,6 +68,18 @@ function App() {
           <button onClick={addTodo} className="btn btn-primary">
             Ajouter
           </button>
+        </div>
+        <div className="space-y-2 flex-1 h-fit">
+          <div className="flex flex-wrap gap-4">
+            <button
+              className={` btn btn-soft ${
+                filter === "Tous" ? "btn-primary" : ""
+              }`}
+              onClick={() => setFilter("Tous")}
+            >
+              Tous
+            </button>
+          </div>
         </div>
       </div>
     </div>
